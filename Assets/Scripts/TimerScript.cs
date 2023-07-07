890c23a3d679fa722af55a733da97cd642b6d45b
VanDany <psr10701@students.ephec.be>
2023-07-07 10:23:49 +0200

    UPDATE inversion progressBar

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -9,20 +9,30 @@ public class TimerScript : MonoBehaviour
     public Image timerBar;
     public float MaxTime = Constants.MaxTime;
     float timeLeft;
+    private int currentLevel = 1;
     // Start is called before the first frame update
     void Start()
     {
         timerBar = GetComponent<Image>();
         timeLeft = MaxTime;
+        PlayerInfos.pi.levelTxt.text = "Level : " + currentLevel;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timeLeft > 0)
+        if (timeLeft <= MaxTime)
         {
             timeLeft -= Time.deltaTime;
-            timerBar.fillAmount = timeLeft / MaxTime;
+            float percent = 1 - (timeLeft / MaxTime);
+            timerBar.fillAmount = 1 - (timeLeft / MaxTime);
+            if (timeLeft <= 0)
+            {
+                timeLeft = MaxTime;
+                currentLevel += 1;
+                PlayerInfos.pi.levelTxt.text = "Level : " + currentLevel;
+            }
         }
+        
     }
 }
